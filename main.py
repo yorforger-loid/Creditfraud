a2ad898991c66a5f1c6d12eebec75a9fde4312a5
yorforger-loid <yashwinireddy0826@gmail.com>
2026-04-24 19:35:32 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import joblib
+import numpy as np
+
+app = FastAPI()
+
+model = joblib.load("fraud_model.pkl")
+
+@app.get("/")
+def home():
+    return {"message": "Fraud Detection API running"}
+
+@app.post("/predict")
+def predict(data: dict):
+    input_data = np.array(data["input"]).reshape(1, -1)
+    prediction = model.predict(input_data)
+    
+    return {"prediction": int(prediction[0])}
