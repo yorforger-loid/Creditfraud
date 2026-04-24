c28f2a2a26a40860e20c69f4a9a94206438a35f3
yorforger-loid <yashwinireddy0826@gmail.com>
2026-04-25 00:57:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # for demo
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 from fastapi import FastAPI
 import joblib
 import numpy as np
